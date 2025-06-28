c30cf47f8238c24eb4f3b0b867487f1c006309c4
Amarthyanath B <amarthyanathb@Amar-MacPro.local>
2025-06-28 21:49:01 +0530

    Server test

--- backend/app.py
+++ backend/app.py
@@ -167,3 +167,5 @@ if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_DEBUG', 'True').lower() == 'true'
     port = int(os.getenv('PORT', 5000))
     app.run(debug=debug_mode, port=port, host='0.0.0.0')
+
+#CD test
